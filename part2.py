5f2fad5f1f83e87323bdaf53bf099918bc0e8342
Ioanna Papagianni <ioannapap@users.noreply.github.com>
2019-04-18 16:14:51 +0300

    Add files via upload

--- part2.py
+++ part2.py
@@ -42,21 +42,26 @@ def windowEvaluation(d,b):								#xLow:d[0]	xHigh:d[1]	yLow:d[2]	yHigh:d[3]
 			upperXCellBound=float(b[0])+((x+1)*dividedRangeX)
 			lowerYCellBound=float(b[2])+(y*dividedRangeY)
 			upperYCellBound=float(b[2])+((y+1)*dividedRangeY)
+	
+			print('cell (%d ,%d)' % (x,y))
+			print(lowerXCellBound)
+			print(upperXCellBound)
+			print(lowerYCellBound)
+			print(upperYCellBound)
 			
-			if d[0]>=lowerXCellBound
-			#print('cell (%d ,%d)' % (x,y))
-			#print(lowerXCellBound)
-			#print(upperXCellBound)
-			#print(lowerYCellBound)
-			#print(upperYCellBound)
 			if d[0]==lowerXCellBound and d[1]==upperXCellBound and d[2]==lowerYCellBound and d[3]==upperYCellBound:
+				print('############################################1')
 				#1
 			elif d[0]>lowerXCellBound and d[1]<upperXCellBound and d[2]>lowerYCellBound and d[3]<upperYCellBound:
+				print('############################################2')
 				#2
 			elif d[0]>=lowerXCellBound and d[0]<upperXCellBound and d[1]>upperXCellBound and d[2]>=lowerYCellBound and d[2]<upperYCellBound and d[3]>upperYCellBound:
+				print('######################3#####################3')
 				#3
 			elif d[0]<lowerXCellBound and d[1]<=upperXCellBound and d[1]>lowerXCellBound and d[2]<lowerYCellBound and d[3]<=upperYCellBound and d[3]>lowerYCellBound:
+				print('############################################4')
 				#4
+			
 def getWindow(b):
 	print('---------------------------WINDOW SELECTION QUERY---------------------------')
 	checked=0
@@ -67,14 +72,16 @@ def getWindow(b):
 			#if not float goes right away to except ValueError --> it skips check=checkDimensions
 			checked=checkDimensions(dimensions, b)
 		except ValueError:
-			checked=0				
+			checked=0	
+	return dimensions			
 
 if __name__ == '__main__':
 
 	dirList=[] 			#list of float lists
-	dimensions=[]		#float list
+	
 	bounds=dirData() 	#string list
-	getWindow(bounds)
+	dimensions=getWindow(bounds)
 	#print(dirList)		[[0, 0, 0, 108], [0, 1, 2894, 179], [0, 2, 7688, 20],...]
+	
 	windowEvaluation(dimensions, bounds)
 	
