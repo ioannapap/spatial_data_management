ca1423930d66244fc918d4541fd78779ac8e13da
Ioanna Papagianni <ioannapap@users.noreply.github.com>
2019-04-20 21:09:41 +0300

    Add files via upload

--- part2.py
+++ part2.py
@@ -1,7 +1,4 @@
-import sys
-import math
-
-def checkDimensions(d, b): 								#xLow:d[0]	xHigh:d[1]	yLow:d[2]	yHigh:d[3]
+def checkDimensions(d, b): 								
 	
 	minX=float(b[0])
 	maxX=float(b[1])
@@ -16,10 +13,9 @@ def checkDimensions(d, b): 								#xLow:d[0]	xHigh:d[1]	yLow:d[2]	yHigh:d[3]
 def dirData():
 
 	firstRow=1
+
 	with open('grid.dir', 'r', encoding='UTF-8') as dfdir:
-		
 		for row in dfdir:	
-			
 			if firstRow==1:
 				boundaries=row.split(' ')
 				firstRow=0
@@ -27,9 +23,10 @@ def dirData():
 				row=row.split(' ')
 				intRow=[int(i) for i in row]	
 				dirList.insert(len(dirList), intRow)
+	
 	return boundaries
 
-def windowEvaluation(d,b):								#xLow:d[0]	xHigh:d[1]	yLow:d[2]	yHigh:d[3]
+def windowEvaluation(d,b):							
 	
 	founddl=0
 	foundur=0
@@ -38,82 +35,81 @@ def windowEvaluation(d,b):								#xLow:d[0]	xHigh:d[1]	yLow:d[2]	yHigh:d[3]
 
 	for x in range(10):
 		for y in range(10):
-			count=0
+			
 			lowerXCellBound=float(b[0])+(x*dividedRangeX)
 			upperXCellBound=float(b[0])+((x+1)*dividedRangeX)
 			lowerYCellBound=float(b[2])+(y*dividedRangeY)
 			upperYCellBound=float(b[2])+((y+1)*dividedRangeY)
 			
 			if d[0]>=lowerXCellBound and d[0]<upperXCellBound and d[2]>=lowerYCellBound and d[2]<upperYCellBound:
-				print('cell (%d ,%d)' % (x,y))
-				founddl=1
-				
+				print('lower left corner cell (%d ,%d)' % (x,y))
+				founddl=1	
 				xdl=x
 				ydl=y
 
 			if d[1]<=upperXCellBound and d[1]>lowerXCellBound and d[3]<=upperYCellBound and d[3]>lowerYCellBound:
-				print('cell (%d ,%d)' % (x,y))
-				foundur=1
-				
+				print('upper right corner cell (%d ,%d)' % (x,y))
+				foundur=1	
 				xur=x
 				yur=y
 
 			if founddl==1 and foundur==1:
-				with open('grid.grd', 'r', encoding='UTF-8') as dfgrd: 
+				with open('grid.grd', 'r', encoding='UTF-8') as dfgrd, open('results_part2.txt', 'w', encoding='UTF-8') as rp2: 
 					for l in dirList:
 						numspots=0
-						if l[0]>xdl and l[0]<xur and l[1]>ydl and l[1]<yur: 
-							#inside cells
+						
+						if l[0]>xdl and l[0]<xur and l[1]>ydl and l[1]<yur and numspots==0: 
+							print('inside cell (%d ,%d)\n' % (l[0],l[1]))
 							dfgrd.seek(l[2])
+							
 							for row in dfgrd:
-								if numspots<=l[3]:
+								
+								if numspots<l[3]:
 									numspots+=1
 									row=row.split(' ')
-									print(row[0]+' '+row[1]+' '+row[2])
-									count+=1
+									print(row[0]+' '+row[1]+' '+row[2])	
+									rp2.write('%s %s %s' % (row[0], row[1], row[2]))
 								else:
-									numspots=0
 									break
 
-						elif ((l[0]==xdl or l[0]==xur) and l[1]>=ydl and l[1]<=yur) or ((l[1]==ydl or l[1]==yur) and l[0]>=xdl and l[0]<=xur):
-							#the cells that are on the window's lines
+						elif ((l[0]==xdl or l[0]==xur) and l[1]>=ydl and l[1]<=yur) or ((l[1]==ydl or l[1]==yur) and l[0]>=xdl and l[0]<=xur) and numspots==0:
+							print('crossed by window"s line cell (%d ,%d)\n' % (l[0],l[1]))
 							dfgrd.seek(l[2])
+							
 							for row in dfgrd:
 								row=row.split(' ')
-
+								numspots+=1
 								if float(row[1])>=d[0] and float(row[1])<=d[1] and float(row[2])>=d[2] and float(row[2])<=d[3] and numspots<l[3]:
-									numspots+=1
+									
 									print(row[0]+' '+row[1]+' '+row[2])
-									count+=1
-								elif numspots>=l[3]:
-									numspots=0 
+									rp2.write('%s %s %s' % (row[0], row[1], row[2]))
+								if numspots>=l[3]:
 									break
+
 				break
-		print(count)
+		
 		if founddl==1 and foundur==1:
 			break
 
 
 def getWindow(b):
+	
 	print('---------------------------WINDOW SELECTION QUERY---------------------------')
 	checked=0
 	while checked==0 or len(dimensions)!=4:
-		dimensions=input("Give LowerX, UpperX (%s-%s) and LowerY, UpperY (%s-%s):\n" % (b[0], b[1], b[2], b[3]) ).split(' ')	
+		dimensions=input('Give LowerX, UpperX (%s-%s) and LowerY, UpperY (%s-%s): ' % (b[0], b[1], b[2], b[3]) ).split(' ')	
 		try: 
 			dimensions=[float(i) for i in dimensions]
-			#if not float goes right away to except ValueError --> it skips check=checkDimensions
 			checked=checkDimensions(dimensions, b)
 		except ValueError:
 			checked=0	
+	
 	return dimensions
 
 if __name__ == '__main__':
 
-	dirList=[] 			#list of float lists
-	
-	bounds=dirData() 	#string list
+	dirList=[] 				
+	bounds=dirData() 	
 	dimensions=getWindow(bounds)
-	#print(dirList)		[[0, 0, 0, 108], [0, 1, 2894, 179], [0, 2, 7688, 20],...]
-	
 	windowEvaluation(dimensions, bounds)
 	
